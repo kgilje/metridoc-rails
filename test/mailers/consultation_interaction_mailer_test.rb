72fa9adfe23780f62e35598b3345f7b08c779896
Karin Gilje <karin.e.gilje@gmail.com>
2022-03-14 15:08:03 -0400

    Testing structure for the consultation instruction mailer.

---
+++ test/mailers/consultation_interaction_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ConsultationInteractionMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/mailers/previews/consultation_interaction_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/consultation_interaction_mailer
+class ConsultationInteractionMailerPreview < ActionMailer::Preview
+
+end
